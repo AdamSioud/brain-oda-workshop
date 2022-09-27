e54f118407d1cc6017123b1df0a1a4bff6b5548e
Lars Mushom <32278242+larsmus@users.noreply.github.com>
2022-09-27 15:21:43 +0200

    add files

---
+++ utils.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import json
+
+
+def load_model():
+    model = tf.keras.models.load_model("transformer_N10")
+    return model
+
+
+def load_seed_products():
+    with open("dinner_dictionary") as json_file:
+        data = json.load(json_file)
+
+
+def load_products_df():
+    return pd.read_csv("products.csv")
+
+
+def load_category_mapping():
+    return pd.read_csv("categories.csv")
+
+
+def create_idx_to_id_mapping():
+    product_df = load_products_df()
+    mapping = dict(zip(product_df.product_idx, product_df.product_id))
+    return mapping
+
+
+def create_id_to_idx_mapping():
+    product_df = load_products_df()
+    mapping = dict(zip(product_df.product_id, product_df.product_idx))
+    return mapping
+
+
+def create_id_to_name_mapping():
+    product_df = load_products_df()
+    mapping = dict(zip(product_df.product_id, product_df["name"]))
+    return mapping
+
+
+def create_idx_to_name_mapping():
+    product_df = load_products_df()
+    mapping = dict(zip(product_df.product_idx, product_df["name"]))
+    return mapping
+
+def create_idx_to_category_two_mapping():
+    category_df = load_category_mapping()
+    mapping = dict(
+        zip(category_df.product_idx, category_df.product_category_name_level_two)
+    )
+    return mapping
+
+
+def predict_next(model: tf.keras.Model, inputs: list, k: int = 1):
+    inputs = np.array(inputs).reshape((1, -1))
+    pred = model(inputs)
+    pred = tf.nn.softmax(tf.reshape(pred[:, -1, :], (-1))).numpy()
+
+    sorted_preds = np.argsort(-pred)
+
+    idx = sorted_preds[:k]
+    scores = pred[idx]
+
+    return idx, scores
